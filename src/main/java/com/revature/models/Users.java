a3a847f352fcd7d7460687b10c78e161242e613a
jkof86 <jason.kofi@revature.net>
2022-02-03 16:45:39 -0500

    Added basic framework

---
+++ src/main/java/com/revature/models/Users.java
@@ -0,0 +1,67 @@
+package com.revature.models;
+
+public class Users {
+    private int id;
+    private String username;
+    private String password;
+    private String fname;
+    private String lname;
+    private String email;
+    private int roleId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+}
